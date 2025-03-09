b1a0ea95652dee6a0de01c1e8a563e81e5eec83e
edaniKE <erdos.daniel.noe@students.jedlik.eu>
2025-03-09 13:15:58 +0100

    Pages

---
+++ resources/js/Components/Pages/HomePage.vue
@@ -0,0 +1,21 @@
+    <script>
+    import NewsList from '@/Components/Pages/NewsList.vue';
+    
+    export default {
+      name: 'Home',
+      components: {
+        NewsList,
+      },
+    }
+    </script>
+
+<template>
+  <div>
+    <NewsList />
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
