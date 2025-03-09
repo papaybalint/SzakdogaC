54685e26895089263aa35027cfc6abf8d82c903e
edaniKE <erdos.daniel.noe@students.jedlik.eu>
2025-03-09 15:16:00 +0100

    Javítás

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -22,39 +22,14 @@ defineProps({
     },
 });
 
-function handleImageError() {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    document.getElementById('background')?.classList.add('!hidden');
-}
 </script>
 
 <template>
-    <Head title="Welcome" />
+    <Head title="HomePage" />
     
-    <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+    <div>
         <header>
             <NavBar/>
-            <!-- <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
-                <Link v-if="$page.props.auth.user" :href="route('dashboard')"
-                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                Dashboard
-            </Link>
-            
-            <template v-else>
-                    <Link :href="route('login')"
-                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                    Log in
-                    </Link>
-
-                    <Link v-if="canRegister" :href="route('register')"
-                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                    Register
-                    </Link>
-                    
-                </template>
-            </nav> -->
         </header>
 
         <main class="mt-6">
