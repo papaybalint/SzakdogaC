851625f196ae96debb325096bb26c0e6ac1749ab
Balint Papay <ppapay2@gmail.com>
2025-03-12 22:18:27 +0100

    Ne torodjetek vele, csak a kikommentelt reszt hagyjatok bent egyelore

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,31 +1,3 @@
-// import '../css/app.css';
-// import './bootstrap';
-
-// import { createInertiaApp } from '@inertiajs/vue3';
-// import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-// import { createApp, h } from 'vue';
-// import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-
-// const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
-// createInertiaApp({
-//     title: (title) => `${title} - ${appName}`,
-//     resolve: (name) =>
-//         resolvePageComponent(
-//             `./Pages/${name}.vue`,
-//             import.meta.glob('./Pages/**/*.vue'),
-//         ),
-//     setup({ el, App, props, plugin }) {
-//         return createApp({ render: () => h(App, props) })
-//             .use(plugin)
-//             .use(ZiggyVue)
-//             .mount(el);
-//     },
-//     progress: {
-//         color: '#4B5563',
-//     },
-// });
-
 import '../css/app.css';
 import './bootstrap';
 
@@ -40,8 +12,8 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
-            `./Components/Pages/${name}.vue`,
-            import.meta.glob('./Components/Pages/**/*.vue'),
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
@@ -54,4 +26,32 @@ createInertiaApp({
     },
 });
 
-//Ez utobbit meg ne torold ki pls.
+// import '../css/app.css';
+// import './bootstrap';
+
+// import { createInertiaApp } from '@inertiajs/vue3';
+// import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+// import { createApp, h } from 'vue';
+// import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+// const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+// createInertiaApp({
+//     title: (title) => `${title} - ${appName}`,
+//     resolve: (name) =>
+//         resolvePageComponent(
+//             `./Components/Pages/${name}.vue`,
+//             import.meta.glob('./Components/Pages/**/*.vue'),
+//         ),
+//     setup({ el, App, props, plugin }) {
+//         return createApp({ render: () => h(App, props) })
+//             .use(plugin)
+//             .use(ZiggyVue)
+//             .mount(el);
+//     },
+//     progress: {
+//         color: '#4B5563',
+//     },
+// });
+
+//Ez utobbit meg ne torold ki pls -B.
