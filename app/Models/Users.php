740daee5f6da3c49ff15a11b6ac0de1ab3503294
zolee <zolee6@gmail.com>
2025-04-07 13:03:31 +0200

    users

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -28,5 +28,32 @@ class Users extends Model
     {
         return $this->belongsTo(Borrowing::class);
     }
+
+     /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+
+   
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
 
